651d360c9bd38c8c2b3b6bfe86ca23fffa007dd0
Ryo Khrisna Fitriawan <ryokhrisnaf@gmail.com>
2023-06-07 13:27:40 +0700

    fix menu page selesai

--- lib/screens/home/menu_page.dart
+++ lib/screens/home/menu_page.dart
@@ -13,64 +13,73 @@ class MenuPage extends StatelessWidget {
   }
 
   Widget categoryBox() {
-    return Stack(
-      children: [
-        Container(
-          decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10),
-              image: DecorationImage(
-                  image: NetworkImage(
-                      'https://source.unsplash.com/random/1500×1500/?programming'))),
-        ),
-        Container(
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(10),
-            gradient: LinearGradient(
-              begin: Alignment.bottomCenter,
-              end: Alignment.topCenter,
-              stops: [
-                0.05,
-                0.7,
-              ],
-              colors: [
-                Colors.black87,
-                Colors.transparent,
-              ],
+    return GestureDetector(
+      onTap: () {
+        
+      },
+      child: Container(
+        child: Stack(
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  image: DecorationImage(
+                      fit: BoxFit.cover,
+                      image: NetworkImage(
+                        'https://source.unsplash.com/random/1500×1500/?programming',
+                      ))),
             ),
-          ),
-        ),
-        Positioned(
-          bottom: 0,
-          child: Container(
-            margin: EdgeInsets.all(10),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  'Programming',
-                  style: whiteText.copyWith(fontSize: 17, fontWeight: bold),
+            Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                gradient: LinearGradient(
+                  begin: Alignment.bottomCenter,
+                  end: Alignment.topCenter,
+                  stops: [
+                    0.3,
+                    0.7,
+                  ],
+                  colors: [
+                    Colors.black87,
+                    Colors.transparent,
+                  ],
                 ),
-                Text(
-                  'berita programming terkini',
-                  style: whiteText.copyWith(fontSize: 13),
-                ),
-              ],
+              ),
             ),
-          ),
-        )
-      ],
+            Positioned(
+              bottom: 0,
+              child: Container(
+                margin: EdgeInsets.all(10),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Programming',
+                      style: whiteText.copyWith(fontSize: 17, fontWeight: bold),
+                    ),
+                    Text(
+                      'berita programming terkini',
+                      style: whiteText.copyWith(fontSize: 13),
+                    ),
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: EdgeInsets.all(15),
+      margin: EdgeInsets.all(10),
       child: GridView.count(
-        mainAxisSpacing: 15,
-        crossAxisSpacing: 15,
         crossAxisCount: 2,
+        mainAxisSpacing: 10,
+        crossAxisSpacing: 10,
         children: [
           categoryBox(),
           categoryBox(),
